6b80bb27f532f267b5fa7b507e7b8539c0ca56d3
kohei <kohei@dayaman.local>
2017-10-20 20:38:43 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import twython
+from datetime import datetime as dt
+
+mona = requests.get('https://api.zaif.jp/api/1/last_price/mona_jpy')
+
+crypts = mona.json()
+
+with open("kakaku.txt", "w")as file:
+    file.write(str(crypts["last_price"]))
+
+with open("twi.api", "r")as ap:
+    API_KEY = ap.readline().strip()
+    API_SEC = ap.readline().strip()
+    TOC = ap.readline().strip()
+    TOC_KEY = ap.readline().strip()
+
+api = twython.Twython(app_key=API_KEY,
+                      app_secret=API_SEC,
+                      oauth_token=TOC,
+                      oauth_token_secret=TOC_KEY)
+now = dt.now()
+api.update_status(status="現在のMONACOINの価格 : "
+                  +str(crypts["last_price"])
+                  +" 円\n"
+                  +now.strftime('(%H:%M現在)'))
