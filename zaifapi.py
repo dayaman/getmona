6bb1060b5c139525be2121c65f1592c347c07d0f
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-04-19 15:52:33 +0900

    価格取得とりあえず変更

--- zaifapi.py
+++ zaifapi.py
@@ -1,19 +1,40 @@
-import time
+import threading
+from time import sleep
 import json
 import websocket
 
-def getjpy():
-    ws_jpy=websocket.create_connection('wss://ws.zaif.jp:8888/stream?currency_pair=mona_jpy')
-    ws_jpy.send('payload')
-    result=json.loads(ws_jpy.recv())
-    price_jpy=result['last_price']['price']
-    ws_jpy.close()
-    return price_jpy
+price={}
 
-def getbtc():
-    ws_btc=websocket.create_connection('wss://ws.zaif.jp:8888/stream?currency_pair=mona_btc')
-    ws_btc.send('payload')
-    result=json.loads(ws_btc.recv())
-    price_btc=result['last_price']['price']
-    ws_jpy.close()
-    return price_btc
+def main():
+    thread_a=threading.Thread(target=getprice, args=('mona_jpy',))
+    thread_a.start()
+    getprice('mona_btc')
+    
+def getprice(pair):
+    global price
+    name=pair
+    thread1=threading.Thread(target=connws, args=(name,))
+    thread1.start()
+    sleep(5)
+    while True:
+        rec_price(price[name])
+        sleep(1)
+
+def rec_price(rc):
+    money=rc['last_price']['price']
+    pair_name=rc['currency_pair']
+    f = open(pair_name+'.txt', 'w')
+    f.write(str(money))
+    f.close()
+    
+def connws(pair):
+    global price
+    ws=websocket.create_connection('wss://ws.zaif.jp:8888/stream?currency_pair='+pair)
+    while True:
+        try:
+            ws.send('payload')
+            price[pair]=json.loads(ws.recv())
+        except:
+            ws=websocket.create_connection('wss://ws.zaif.jp:8888/stream?currency_pair='+pair)
+        sleep(1)
+        
