872a101442c992f58d178713c06ec8c89ec77bdb
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-04-20 19:36:52 +0900

    いらないものや部分を消した

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-import requests
-#import twython
 import tweet
 from datetime import datetime as dt
-
-mona = requests.get('https://api.zaif.jp/api/1/last_price/mona_jpy')
-monabtc = requests.get('https://api.zaif.jp/api/1/last_price/mona_btc')
-
-crypts = mona.json()
-crypts_btc = monabtc.json()
-
-# with open("twi.api", "r")as ap:
-#     API_KEY = ap.readline().strip()
-#     API_SEC = ap.readline().strip()
-#     TOC = ap.readline().strip()
-#     TOC_KEY = ap.readline().strip()
-
-# api = twython.Twython(app_key=API_KEY,
-#                       app_secret=API_SEC,
-#                       oauth_token=TOC,
-#                       oauth_token_secret=TOC_KEY)
 
 nowmona = crypts["last_price"]
 nowmona_btc = crypts_btc["last_price"]
