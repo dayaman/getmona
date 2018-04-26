da087f611e64fc0fd420326cbeb4a2e1334c2dce
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-04-26 19:32:05 +0900

    とりあえず書いた(未検証)

--- getmona/seeds.py
+++ getmona/seeds.py
@@ -2,7 +2,11 @@ from getmona import db
 from getmona.models import Price
 
 def addpair():
-    pass
+    add_to_db("mona_jpy")
+    add_to_db("mona_btc")
 
-def add_to_db(pair):
-    pass
+def add_to_db(nmoney):
+    price = Price(pair=nmoney)
+    db.session.add(price)
+    db.session.commit()
+
