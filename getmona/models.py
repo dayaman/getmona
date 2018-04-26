e8976400ac1988692485de678997770996fa4873
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-04-26 15:11:23 +0900

    dbとりあえずできた

--- getmona/models.py
+++ getmona/models.py
@@ -1,13 +1,14 @@
 from getmona import db
 
-class Latest_Price(db.Model):
-    __tablename__ = 'latest_prices'
+class Price(db.Model):
+    __tablename__ = 'prices'
     pair = db.Column(db.String, primary_key=True)
-    price = db.Column(db.Float)
+    latest_price = db.Column(db.Float)
+    yesterday_price = db.Column(db.Float)
 
     def __repr__(self):
-        return '<pair={pair} price={price!r}>'.format(
-            pair=self.pair, price=self.price)
+        return '<pair={pair} latest_price={latest_price} yesterday_price={yesterday_price}>'.format(
+            pair=self.pair, latest_price=self.latest_price, yesterday_price=self.yesterday_price)
 
 def init():
     db.create_all()
