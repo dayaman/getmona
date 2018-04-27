67a4317eaa02e1c7249d4ca15b7dc1d6d9726457
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-04-28 00:41:10 +0900

    ueshitaを極力綺麗にしたいけども

---
+++ getmona/ueshita.py
@@ -0,0 +1,27 @@
+import time
+from datetime import datetime
+from getmona import db
+from getmona.models import Price
+from getmona.tweet import tweet
+
+def loop_get():
+    mae_jpy = 0
+    mae_btc = 0
+    while True:
+        mae_jpy = get_ratio('mona_jpy', mae_jpy)
+        mae_btc = get_ratio('mona_btc', mae_btc)
+        sleep(1)
+        
+def get_ratio(nmoney, mae):
+    mona = Price.query.get(nmoney)
+    per = ((mona.latest_price / mona.yesterday_price) * 100 - 100) // 5
+    judge = is_Tweet(per, mae)
+
+def is_Tweet(per, mae):
+    if per > 0:
+        if mae < 0:
+            return True
+        if per - mae >= 1:
+            return True
+    elif per < 0:
+        
